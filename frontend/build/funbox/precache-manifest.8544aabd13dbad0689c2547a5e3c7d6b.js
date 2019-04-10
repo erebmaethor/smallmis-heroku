9e0309bd0eee687276902c60a5ae0cdee25a2a1c
Mikhail Smolin <erebmaethor@gmail.com>
2019-04-10 10:25:27 +0300

    Add funbox

---
+++ frontend/build/funbox/precache-manifest.8544aabd13dbad0689c2547a5e3c7d6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bbf28e5ddd702ebc9002",
+    "url": "/static/js/main.7bb0162e.chunk.js"
+  },
+  {
+    "revision": "5cda5deb973af749b4b8",
+    "url": "/static/js/2.f3fe1ca1.chunk.js"
+  },
+  {
+    "revision": "bbf28e5ddd702ebc9002",
+    "url": "/static/css/main.e980f571.chunk.css"
+  },
+  {
+    "revision": "8ea9f1cbb1010aeb68f72f0019676704",
+    "url": "/index.html"
+  }
+];
