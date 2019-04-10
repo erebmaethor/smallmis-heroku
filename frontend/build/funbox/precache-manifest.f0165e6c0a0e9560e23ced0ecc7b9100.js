9eb4180ea28199a87e477b5f389831526ab76994
Mikhail Smolin <erebmaethor@gmail.com>
2019-04-10 10:46:44 +0300

    Second try to add funbox

---
+++ frontend/build/funbox/precache-manifest.f0165e6c0a0e9560e23ced0ecc7b9100.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a937d60f6e788f0f21a5",
+    "url": "/funbox/static/js/runtime~main.65744a31.js"
+  },
+  {
+    "revision": "86215c9a5136da1e4a5f",
+    "url": "/funbox/static/js/main.deb9ff8a.chunk.js"
+  },
+  {
+    "revision": "5cda5deb973af749b4b8",
+    "url": "/funbox/static/js/2.f3fe1ca1.chunk.js"
+  },
+  {
+    "revision": "86215c9a5136da1e4a5f",
+    "url": "/funbox/static/css/main.45d12371.chunk.css"
+  },
+  {
+    "revision": "2c0362f8c33c078b1eefedced7f87961",
+    "url": "/funbox/index.html"
+  }
+];
--- frontend/build/funbox/service-worker.js
+++ frontend/build/funbox/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8544aabd13dbad0689c2547a5e3c7d6b.js"
+  "/funbox/precache-manifest.f0165e6c0a0e9560e23ced0ecc7b9100.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/funbox/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
